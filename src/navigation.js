e5107d9205037d85bebe6a1b77e4e4317290be20
Thomas Hayner <Thom.Hayner@gmail.com>
2024-06-30 12:39:50 -0600

    update landing page

--- src/navigation.js
+++ src/navigation.js
@@ -3,25 +3,11 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      text: 'Welcome',
+      href: getPermalink('/welcome'),
+    },{
+      text: 'saas',
+      href: getPermalink('/saas'),
     },
     {
       text: 'Pages',
