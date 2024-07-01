2ceb32cc141bc3d912e2a90bfd2299e93f62d6b1
Thomas Hayner <Thom.Hayner@gmail.com>
2024-07-01 01:56:22 -0600

    add linkedin (and youtube placeholder

--- src/navigation.js
+++ src/navigation.js
@@ -85,6 +85,8 @@ export const footerData = {
     // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    // { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: '#' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/ScaleForceAgency' },
   ],
