32076d216909d9f13c3fff37179a04f4f0063ac0
Thomas Hayner <Thom.Hayner@gmail.com>
2024-07-04 13:10:27 -0600

    install line-md icon set from iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,6 +28,7 @@ export default defineConfig({
     icon({
       include: {
         tabler: ['*'],
+        'line-md': ['*'],
         'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
       }
     }),
