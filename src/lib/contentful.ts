702281f6734cee0270ad47fe7e5a37860ee62c26
Thomas Hayner <Thom.Hayner@gmail.com>
2024-07-08 13:35:45 -0600

    revert after finding correct API key

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -4,13 +4,8 @@ import contentful from "contentful";
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: "cdn.contentful.com",
-
-  // Below is an alternate arrangement for using the Contently Preview feature
-  // (Preview feature only available to Contently paid plan users)
-  // accessToken: import.meta.env.DEV
-  //   ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-  //   : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  // host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
