3139ca3acdcd1caea60e0ee5cfb4d0e051260bf9
Thomas Hayner <Thom.Hayner@gmail.com>
2024-07-10 22:09:59 -0600

    update types

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,6 +1,86 @@
 // https://docs.astro.build/en/guides/cms/contentful/#installing-dependencies
 
 import contentful from "contentful";
+import type { EntryFieldTypes, Asset } from "contentful";
+
+export interface BlogPost {
+  contentTypeId: "blogPost",
+  fields: {
+    title: EntryFieldTypes.Text;
+    content: EntryFieldTypes.RichText;
+    date: EntryFieldTypes.Date;
+    description: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+  }
+}
+
+export interface LandingImage {
+  contentTypeId: "image",
+  fields: {
+    title: EntryFieldTypes.Text;
+    alt: EntryFieldTypes.Text;
+    file: Asset;
+  }
+};
+
+export interface LandingPage {
+  contentTypeId: "landingPage";
+  fields: {
+    identifier: EntryFieldTypes.Text;
+    metadata: Metadata;
+    hero: Component;
+    services: Component[];
+    features: Component[];
+    faq: Component;
+  };
+};
+
+interface Metadata {
+  contentTypeId: "metadata",
+  fields: {
+    identifier: EntryFieldTypes.Text;
+  }
+};
+
+interface TitleBlock {
+  contentTypeId: "titleBlock";
+  fields: {
+    identifier: EntryFieldTypes.Text;
+    tagline: EntryFieldTypes.Text;
+    title: EntryFieldTypes.Text;
+    subtitle: EntryFieldTypes.Text;
+  }
+};
+
+interface Card {
+  contentTypeId: "card";
+  fields: {
+    identifier: EntryFieldTypes.Text;
+    title: EntryFieldTypes.Text;
+    description: EntryFieldTypes.Text;
+    icon: EntryFieldTypes.Symbol;
+  }
+};
+
+interface FaqCard {
+  contentTypeId: "faqCard";
+  fields: {
+    identifier: EntryFieldTypes.Text;
+    question: EntryFieldTypes.Text;
+    answer: EntryFieldTypes.Text;
+    icon: EntryFieldTypes.Symbol;
+  }
+};
+
+interface Component {
+  contentTypeId: "component";
+  fields: {
+    identifier: EntryFieldTypes.Text;
+    sectionHeader: TitleBlock;
+    titleBlockContent: TitleBlock;
+    cards: Card[] | FaqCard[];
+  }
+};
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
