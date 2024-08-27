f45d2a64ad2f6d713361b6c2a3e789b0de139b66
Thomas Hayner <Thom.Hayner@gmail.com>
2024-08-27 16:47:24 -0600

    add index.ts for package

---
+++ src/integrations/google-tag-manager/index.ts
@@ -0,0 +1,5 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+export { default as GoogleTagManagerHead } from "./src/GoogleTagManagerHead.astro";
+export { default as GoogleTagManagerBody } from "./src/GoogleTagManagerBody.astro";
